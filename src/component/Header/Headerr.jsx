356b043ba0a95d223d2fafd0e0ab774d5985bf3e
tcodes317 <tcodes317@gmail.com>
2024-01-17 00:00:02 -0800

    1/16/2024

---
+++ src/component/Header/Headerr.jsx
@@ -0,0 +1,26 @@
+function Headerr(){
+    return(
+        <>
+            <div className="xlg:relative">
+                <div className="xlg:flex xlg:justify-between">
+                    <div className="xlg:flex xlg:justify-between xlg:flex-col">
+                        <h1>Instantly connect with senior tech talent</h1>
+                        <div>Hire tech talent that gets promoted</div>
+                        <div className="">
+                            <button>
+                                <span>Find Candidate Now</span>
+                            </button>
+                        </div>
+                        <div className="">
+
+                        </div>
+                    </div>
+                    <div>
+                        file
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Headerr;
