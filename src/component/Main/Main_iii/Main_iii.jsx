455f933658d6abe4c9bbb5150c9b27891899ace9
tcodes317 <tcodes317@gmail.com>
2024-01-17 20:23:40 -0800

    1/17/2024

--- src/component/Main/Main_iii/Main_iii.jsx
+++ src/component/Main/Main_iii/Main_iii.jsx
@@ -5,27 +5,27 @@ function Main_iii(){
     return(
         <>
         <div className="xlg:relative xlg:top-14">
-            <h1 className="xlg:text-center xlg:font-bold xlg:text-4xl">Get qualified global tech talent in minutes</h1>
-            <div className="xlg:text-center xlg:mt-3">Browse 400K+ World-Class Tech Talent Actively Seeking New Opportunites</div>
-            <div className="xlg:mt-20">
-                <div className="xlg:w-6/12 xlg:p-4 xlg:rounded-full xlg:flex xlg:mx-auto xlg:items-center xlg:justify-between xlg:shadow-xlg xlg:border-2">
+            <h1 className="xlg:text-center xlg:font-bold xlg:text-4xl xxxs:text-2xl xxxs:text-center xxxs:mt-10 xxxs:font-bold xxxs:mb-4">Get qualified global tech talent in minutes</h1>
+            <div className="xlg:text-center xlg:mt-3 xxxs:text-center">Browse 400K+ World-Class Tech Talent Actively Seeking New Opportunites</div>
+            <div className="xlg:mt-20 xxxs:mt-20 xxxs:mb-20">
+                <div className="xlg:w-6/12 xlg:p-4 xxxs:p-5 xxxs:flex xxxs:items-center xlg:rounded-full xxxs:rounded-full xlg:flex xlg:mx-auto xlg:items-center xlg:justify-between xlg:shadow-xlg xxxs:shadow-xlg xlg:border-2 xxxs:border-2">
                     <input type="search" placeholder="Search by skill set" className="w-full"/>
                     <FaSearch />
                 </div>
             </div>
         </div>
         <div className="xlg:mt-20 xlg:mb-16">
-            <div className="xlg:inline-flex xlg:items-center xlg:overflow-x-hidden xlg:space-x-8 xlg:w-full">
-                <div className="xlg:w-10/12 xlg:mt-12"> {/** card */}
-                    <div id="img" className="xlg:w-full xlg:h-8 border-2 i">
+            <div className="xlg:inline-flex xxxs:inline-flex xlg:items-center xlg:overflow-x-hidden xxxs:overflow-x-hidden xlg:space-x-8 xlg:w-full xxxs:w-full">
+                <div className="xlg:w-10/12 xlg:mt-12 xxxs:w-full"> {/** card */}
+                    <div id="img" className="xlg:w-full xxxs:w-full xlg:h-8 border-2 i">
                         <svg id="svg_i" viewBox="0 0 24 24" role="presentation" className="flex-shrink-0" width="3rem" height="3rem"><path d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M10,16.5L16,12L10,7.5V16.5Z" ></path></svg>
                     </div>
-                    <div className="xlg:mt-4">
-                        <h1 className="xlg:font-bold">Sunil</h1>
-                        <div className="xlg:mt-2 xlg:mb-2">Mobile Engineer from India, Living in india</div>
-                        <div className="xlg:truncate xlg:font-bold xlg:w-11/12">Mobile Development, Swift, Android</div>
+                    <div className="xlg:mt-4 xxxs:mt-4">
+                        <h1 className="xlg:font-bold xxxs:font-bold">Sunil</h1>
+                        <div className="xlg:mt-2 xlg:mb-2 xxxs:mt-2 xxxs:mb-2">Mobile Engineer from India, Living in india</div>
+                        <div className="xlg:truncate xlg:font-bold xlg:w-11/12 xxxs:truncate xxxs:font-bold xxxs:w-11/12">Mobile Development, Swift, Android</div>
                     </div>
-                    <button className="xlg:p-4 xlg:border-2 xlg:border-blue-300 xlg:text-blue-300 xlg:rounded-full xlg:bg-white xlg:w-full xlg:mt-6 xlg:mb-6">Interview Now</button>
+                    <button className="xlg:p-4 xxxs:p-4 xlg:border-2 xxxs:mt-6 xxxs:border-2 xlg:border-blue-300 xxxs:border-blue-300 xlg:text-blue-300 xxxs:text-blue-300 xlg:rounded-full xxxs:rounded-full xlg:bg-white xxxs:bg-white xlg:w-full xlg:mt-6 xlg:mb-6 xxxs:w-full">Interview Now</button>
                 </div>
                 <div className="xlg:w-10/12 xlg:mt-12"> {/** card */}
                     <div id="img" className="xlg:w-full xlg:h-8 border-2 ii">
@@ -71,6 +71,13 @@ function Main_iii(){
                     </div>
                     <button className="xlg:p-4 xlg:border-2 xlg:border-blue-300 xlg:text-blue-300 xlg:rounded-full xlg:bg-white xlg:w-full xlg:mt-6 xlg:mb-6">Interview Now</button>
                 </div>
+            </div>
+            <div className="xxxs:mt-2 xlg:hidden xxxs:flex xxxs:items-center xxxs:justify-center xxxs:space-x-2">
+                <div id="dot" className="active xxxs:bg-blue-300"></div>
+                <div id="dot"></div>
+                <div id="dot"></div>
+                <div id="dot"></div>
+                <div id="dot"></div>
             </div>
         </div>
         </>
